145c0bf484031cae2c04ed2f6b7ae2dcc60128d8
momoxstarx <momo.star@gmail.com>
2021-07-22 05:02:09 -0700

    create new class

--- Controllers/FitnessController.cs
+++ Controllers/FitnessController.cs
@@ -34,8 +34,8 @@ namespace FitnessProject.Controllers
         [Authorize(Roles = "Student, Instructor, Admin")]
         public IActionResult Dashboard()
         {
-            Container container = new Container();
             string UserId = _userManager.GetUserId(User);
+            Container container = new Container();
             container.LoggedUser = _db.users.FirstOrDefault(x => x.Id == UserId);
             container.AllClasses = _db.Classes
             .Include(c => c.Instructor)
@@ -49,12 +49,13 @@ namespace FitnessProject.Controllers
         [Authorize(Roles = "Student, Instructor, Admin")]
         public IActionResult Profile()
         {
-            Container container = new Container();
             string UserId = _userManager.GetUserId(User);
+            Container container = new Container();
             container.LoggedUser = _db.users.FirstOrDefault(x => x.Id == UserId);
             // see if user is instructor
             Instructor teacher = _db.Instructors
             .Include(t => t.User)
+            .Include(t => t.Classes)
             .FirstOrDefault(t => t.UserId == UserId);
             if(teacher != null)
             {
@@ -69,43 +70,45 @@ namespace FitnessProject.Controllers
         }
 
 
-        // [HttpGet("newclass")]
-        // public IActionResult NewClass()
-        // {
-        //     if (!isLoggedIn)
-        //     {
-        //         return RedirectToAction("Index", "Home");
-        //     }
-        //     User u = _db.Users.FirstOrDefault(u => u.UserId == (int)uid);
-        //     Container container = new Container();
-        //     container.LoggedUser = u;
-        //     // show a view with a form
-        //     return View(container);
-        // }
+        [HttpGet("newclass")]
+        [Authorize(Roles = "Instructor, Admin")]
+        public IActionResult NewClass()
+        {
+            string UserId = _userManager.GetUserId(User);
+            Container container = new Container();
+            container.LoggedUser = _db.users.FirstOrDefault(x => x.Id == UserId);
+            return View(container);
+        }
 
-        // [HttpPost("processclass")]
-        // public IActionResult ProcessClass(Container fromForm)
-        // {
-        //     // check wedding date if in future
-        //     Console.WriteLine(fromForm.Class.ClassDate);
-        //     if (fromForm.Class.ClassDate < DateTime.Now)
-        //     {
-        //         ModelState.AddModelError("Class.ClassDate", "Class Date must be in the future");
-        //     }
-        //     if (ModelState.IsValid)
-        //     {
-        //         // all is good
-        //         fromForm.Class.UserId = (int)uid;
-        //         _db.Classes.Add(fromForm.Class);
-        //         _db.SaveChanges();
-        //         Console.WriteLine($"successfully added {fromForm.Class.ClassName} on {fromForm.Class.ClassDate}");
-        //         return RedirectToAction("Dashboard", "Fitness");
-        //     }
-        //     User u = _db.Users.FirstOrDefault(u => u.UserId == (int)uid);
-        //     fromForm.LoggedUser = u;
-        //     // show a view with a form
-        //     return View("NewClass", fromForm);
-        // }
+        [HttpPost("processclass")]
+        [Authorize(Roles = "Instructor, Admin")]
+        public IActionResult ProcessClass(Container fromForm)
+        {
+            // check wedding date if in future
+            string UserId = _userManager.GetUserId(User);
+            Console.WriteLine(fromForm.Class.ClassDate);
+            if (fromForm.Class.ClassDate < DateTime.Now)
+            {
+                ModelState.AddModelError("Class.ClassDate", "Class Date must be in the future");
+            }
+            if (ModelState.IsValid)
+            {
+                Console.WriteLine("made it to 96");
+
+                // all is good
+                Instructor teacher = _db.Instructors.FirstOrDefault(t => t.UserId == UserId);
+                Console.WriteLine($"Instructor ID: {teacher.InstructorId}");
+                fromForm.Class.InstructorId = teacher.InstructorId;
+                _db.Classes.Add(fromForm.Class);
+                _db.SaveChanges();
+                Console.WriteLine($"successfully added {fromForm.Class.ClassName} on {fromForm.Class.ClassDate}");
+                return RedirectToAction("Dashboard", "Fitness");
+            }
+            User u = _db.users.FirstOrDefault(u => u.Id == UserId);
+            fromForm.LoggedUser = u;
+            // show a view with a form
+            return View("NewClass", fromForm);
+        }
         // [HttpGet("class/{classId}")]
         // public IActionResult OneClass(int classId)
         // {
