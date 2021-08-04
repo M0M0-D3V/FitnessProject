a15a899a91c2c0b0f27b9bbc6f78c69a2caa749d
momoxstarx <momo.star@gmail.com>
2021-08-04 06:44:42 -0700

    migrate 3

---
+++ Migrations/20210804134218_Messages3.cs
@@ -0,0 +1,229 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FitnessProject.Migrations
+{
+    public partial class Messages3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Message_AspNetUsers_MessageFromId",
+                table: "Message");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Message_AspNetUsers_UserMessagedId",
+                table: "Message");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Review_Classes_ClassId",
+                table: "Review");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Review_AspNetUsers_UserId",
+                table: "Review");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Review",
+                table: "Review");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Message",
+                table: "Message");
+
+            migrationBuilder.RenameTable(
+                name: "Review",
+                newName: "Reviews");
+
+            migrationBuilder.RenameTable(
+                name: "Message",
+                newName: "Messages");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Review_UserId",
+                table: "Reviews",
+                newName: "IX_Reviews_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Review_ClassId",
+                table: "Reviews",
+                newName: "IX_Reviews_ClassId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Message_UserMessagedId",
+                table: "Messages",
+                newName: "IX_Messages_UserMessagedId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Message_MessageFromId",
+                table: "Messages",
+                newName: "IX_Messages_MessageFromId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Messages",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Messages",
+                nullable: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isRead",
+                table: "Messages",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Reviews",
+                table: "Reviews",
+                column: "ReviewId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Messages",
+                table: "Messages",
+                column: "MessageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AspNetUsers_MessageFromId",
+                table: "Messages",
+                column: "MessageFromId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AspNetUsers_UserMessagedId",
+                table: "Messages",
+                column: "UserMessagedId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Classes_ClassId",
+                table: "Reviews",
+                column: "ClassId",
+                principalTable: "Classes",
+                principalColumn: "ClassId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_AspNetUsers_UserId",
+                table: "Reviews",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AspNetUsers_MessageFromId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AspNetUsers_UserMessagedId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Classes_ClassId",
+                table: "Reviews");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_AspNetUsers_UserId",
+                table: "Reviews");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Reviews",
+                table: "Reviews");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Messages",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "isRead",
+                table: "Messages");
+
+            migrationBuilder.RenameTable(
+                name: "Reviews",
+                newName: "Review");
+
+            migrationBuilder.RenameTable(
+                name: "Messages",
+                newName: "Message");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Reviews_UserId",
+                table: "Review",
+                newName: "IX_Review_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Reviews_ClassId",
+                table: "Review",
+                newName: "IX_Review_ClassId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Messages_UserMessagedId",
+                table: "Message",
+                newName: "IX_Message_UserMessagedId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Messages_MessageFromId",
+                table: "Message",
+                newName: "IX_Message_MessageFromId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Review",
+                table: "Review",
+                column: "ReviewId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Message",
+                table: "Message",
+                column: "MessageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Message_AspNetUsers_MessageFromId",
+                table: "Message",
+                column: "MessageFromId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Message_AspNetUsers_UserMessagedId",
+                table: "Message",
+                column: "UserMessagedId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Review_Classes_ClassId",
+                table: "Review",
+                column: "ClassId",
+                principalTable: "Classes",
+                principalColumn: "ClassId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Review_AspNetUsers_UserId",
+                table: "Review",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
